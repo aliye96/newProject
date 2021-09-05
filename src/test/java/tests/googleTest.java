66adca4fd062150355a4a24c5a1d1c42da7eccf8
Aliye <ali.sultan.9696@mail.ru>
2021-09-06 00:05:33 +0400

    Создан кейс1 Добавлен тест проверки

--- src/test/java/tests/googleTest.java
+++ src/test/java/tests/googleTest.java
@@ -1,16 +1,53 @@
 package tests;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import pages.SearchPage;
+
+import java.util.concurrent.TimeUnit;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class googleTest {
 
-    @Test
-    public void test1() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+    private static WebDriver driver;
+    private static SearchPage searchPage;
+
+    @BeforeAll
+    public static void startBrowser() {
+        System.setProperty(
+                "WebDriver.chrome.driver",
+                "src/test/resources/chromedriver.exe"
+        );
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        driver = new ChromeDriver(options);
+        searchPage = new SearchPage(driver);
+    }
+
+    @BeforeEach
+    public void startCalc() {
         driver.get("http://google.com");
+        searchPage.search("Calculator");
+
+    }
+
+    @Test
+    @DisplayName("Кейс 1")
+    public void test1() {
+        searchPage.input("(1 + 2) * (3) -40 /5");
+        assertAll(
+                () -> assertEquals("(1 + 2) * (3) -40 /5", SearchPage.getCalcMemory()),
+                () -> assertEquals("1", SearchPage.getResult())
+
+        );
+
+    }
+
+    @AfterAll
+    public static void teardown() {
         driver.quit();
     }
 }
